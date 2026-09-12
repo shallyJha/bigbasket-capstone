8abfb486bc2fa517c072ed4a8e46afbeaf2f76f9
shallyJha <shallyjha855@gmail.com>
2026-09-12 19:03:30 +0530

    Add SQL query to retrieve distinct product categories

--- 01_foundations.sql
+++ 01_foundations.sql
@@ -3,3 +3,7 @@ SELECT o.order_id, o.customer_id, c.city, o.order_date, o.amount_inr, o.status
 FROM orders o
 JOIN customers c ON c.customer_id = o.customer_id
 WHERE c.city = 'Bengaluru';
+
+-- 1.4.2 DISTINCT: every distinct category in products
+SELECT DISTINCT category
+FROM products;
